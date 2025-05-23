f3423b186b1c22cd80283a30fe37971664cb7d72
s24015 <s24015@std.it-college.ac.jp>
2025-05-23 11:59:47 +0900

    第49回17問

---
+++ D48Q17.java
@@ -0,0 +1,51 @@
+/*
+public class D48Q17 {
+    class Q17{
+        public static void main(String[] args) {
+            int n = 35;
+            System.out.println(n > 15 && n <= 30);
+        }
+    }
+
+
+    class Q18{
+        public static void main(String[] args) {
+            double baseValue = 3.5;
+            int units = 3;
+
+            double width = baseValue * units;
+            System.out.println(width);
+        }
+    }
+
+
+class Q20{
+    public static void main(String[] args){
+        char c = 0x30;
+        c += 9;
+        System.out.println(c);
+    }
+}
+
+
+class Q23{
+    public static void main(String[] args){
+        int i = 1;
+        for (;;){
+            i = i + 3;
+            if (i < 10) {
+                continue;
+            }else{
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
+
+ */
+
+/*
+d48q17について
+D48は第48回のことを表しています。
+ */

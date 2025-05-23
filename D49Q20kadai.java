818a61ec341ee031b81f99c42926fe9e9a5558d9
s24015 <s24015@std.it-college.ac.jp>
2025-05-23 12:29:05 +0900

    第49回18問

---
+++ D49Q20kadai.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class D49Q20kadai {
+    public static void main(String[] args) {
+        double[][] arr = new double[5][10];
+
+        System.out.println(arr.length + " " + arr[0].length);
+
+    }
+}

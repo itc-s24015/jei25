0148ea0f29e23f95a9fb6ca1640c12fa51f44f34
s24015 <s24015@std.it-college.ac.jp>
2025-07-25 12:16:27 +0900

    第５１回の問６までの課題提出

--- D51kadai.java
+++ D51kadai.java
@@ -1,12 +1,6 @@
 public class D51kadai {
     public static void main(String[] args) {
-        String s = String.valueOf(1.1);
-        String s1 = String.valueOf(2.2);
-        String s2 = String.valueOf(3.3);
-        if (args.length >= 2) {
-            double value = Double.parseDouble(args[1]);
-            double result = Math.ceil(value);
-            System.out.println(result);
-        }
+        double[] members = {1.1,2.2,3.4};
+        System.out.println(Math.ceil(members[1]));
     }
 }
